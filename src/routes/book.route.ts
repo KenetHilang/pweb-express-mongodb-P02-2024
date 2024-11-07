54bc9a7452c78f57aab9041cbb10eaefcd86a8cb
Trenttzzz <azzafarichi2@gmail.com>
2024-11-07 19:17:25 +0700

    fix routing

--- src/routes/book.route.ts
+++ src/routes/book.route.ts
@@ -7,8 +7,8 @@ const bookController = new BookController();
 
 router.post('/',auth, bookController.addBook);
 router.get('/',auth, bookController.getAllBooks);
-router.get('/:id', bookController.getBookById);
-router.patch('/:id', bookController.modifyBook);
-router.delete('/:id', bookController.removeBook);
+router.get('/:id', auth, bookController.getBookById);
+router.patch('/:id', auth, bookController.modifyBook);
+router.delete('/:id', auth, bookController.removeBook);
 
 export default router;
